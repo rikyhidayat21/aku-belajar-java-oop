fe03455833a70e2f7d36fdabeafddaab7cddeb56
rikyhidayat21 <rikyhidayat21@gmail.com>
2023-01-31 09:18:18 +0700

    intro constructor - oop 1 kunal

--- src/com/riky/introduction/Main.java
+++ src/com/riky/introduction/Main.java
@@ -2,6 +2,27 @@ package com.riky.introduction;
 
 public class Main {
     public static void main(String[] args) {
+        Student riky = new Student(13, "riky", 88.8f);
+        System.out.println(riky.rno);
+        System.out.println(riky.name);
+        System.out.println(riky.marks);
+    }
+}
 
+// create new class
+class Student {
+    int rno;
+    String name;
+    float marks;
+
+
+    // we need a way to add the values of the above
+    // properties object by object
+
+    // creating constructor
+    Student(int rno, String name, float marks) {
+        this.rno = rno;
+        this.name = name;
+        this.marks = marks;
     }
 }
