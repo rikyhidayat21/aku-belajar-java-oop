50629b189c0c2ae93dc931381cc00fbdcb12baf3
rikyhidayat21 <rikyhidayat21@gmail.com>
2023-02-01 06:38:23 +0700

    import packages

---
+++ src/com/riky/packages/a/Greeting.java
@@ -0,0 +1,11 @@
+package com.riky.packages.a;
+
+
+import com.riky.packages.b.Message;
+
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("Hello from packages a");
+        Message.message();
+    }
+}
---
+++ src/com/riky/packages/b/Greeting.java
@@ -0,0 +1,7 @@
+package com.riky.packages.b;
+
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("Hello from packages b");
+    }
+}
