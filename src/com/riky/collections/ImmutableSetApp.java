5f84b23a1d9d177b3a5236efeca3c212564f91ea
rikyhidayat21 <rikyhidayat21@gmail.com>
2023-02-04 05:58:27 +0700

    immutable set

---
+++ src/com/riky/collections/ImmutableSetApp.java
@@ -0,0 +1,19 @@
+package com.riky.collections;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ImmutableSetApp {
+    public static void main(String[] args) {
+        Set<String> empty = Collections.emptySet();
+        Set<String> one = Collections.singleton("Riky");
+        Set<String> mutable = new HashSet<>();
+        mutable.add("Hidayat");
+        mutable.add("Paten");
+        Set<String> immutable = Collections.unmodifiableSet(mutable);
+
+        Set<String> set = Set.of("Hi", "da", "yat");
+//        set.remove("da"); // error
+    }
+}
