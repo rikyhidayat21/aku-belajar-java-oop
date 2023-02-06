1de2e2d3819fbb9b7a0e080284621624277a8328
rikyhidayat21 <rikyhidayat21@gmail.com>
2023-02-07 06:21:54 +0700

    pzn oop - method overriding

--- src/com/riky/pzn/oop/VicePresident.java
+++ src/com/riky/pzn/oop/VicePresident.java
@@ -1,5 +1,7 @@
 package com.riky.pzn.oop;
 
 class VicePresident extends Manager {
-
+    void sayHello(String name) {
+        System.out.println("Hi " + name + " my name is " + this.name + " as a VP in ...");
+    }
 }
