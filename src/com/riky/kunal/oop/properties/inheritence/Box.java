3e29d6fcbf4e379918d10742ef817131e07c4cdc
rikyhidayat21 <rikyhidayat21@gmail.com>
2023-02-07 08:34:11 +0700

    kunal oop - inheritence

---
+++ src/com/riky/kunal/oop/properties/inheritence/Box.java
@@ -0,0 +1,36 @@
+package com.riky.kunal.oop.properties.inheritence;
+
+public class Box {
+    double l;
+    double h;
+    double w;
+
+    Box() {
+        this.l = -1;
+        this.h = -1;
+        this.w = -1;
+    }
+
+    // cube
+    Box(double side) {
+        this.w = side;
+        this.l = side;
+        this.h = side;
+    }
+
+    Box(double l, double h, double w) {
+        this.l = l;
+        this.h = h;
+        this.w = w;
+    }
+
+    Box(Box old) {
+        this.h = old.h;
+        this.w = old.w;
+        this.l = old.l;
+    }
+
+    public void information() {
+        System.out.println("Running the box");
+    }
+}
---
+++ src/com/riky/kunal/oop/properties/inheritence/Main.java
@@ -0,0 +1,8 @@
+package com.riky.kunal.oop.properties.inheritence;
+
+public class Main {
+    public static void main(String[] args) {
+        Box box = new Box(4, 2.2, 5);
+        System.out.println(box.l + " " + box.w + " " + box.h);
+    }
+}
